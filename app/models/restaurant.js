e9f37d82e38468779cf993ca5caad51c0268137e
jartek <jartek.91@gmail.com>
2015-03-14 09:54:50 +0530

    App

--- app/models/restaurant.js
+++ app/models/restaurant.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   user_id: DS.belongsTo('user'),
-  menu: DS.belongsTo('menu'),
+  menu_id: DS.belongsTo('menu'),
 });
